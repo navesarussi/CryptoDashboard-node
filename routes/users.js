7b2350182faa33e0fe0c99472ff160e0d6897a7d
Nave <navesarussi@gmail.com>
2023-07-02 16:40:34 +0300

    not work 11

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+// const router = express.Router()
+// const { 
+//     addSymbol, 
+//     welcome, 
+//     dashboard, 
+//     logout,
+// } = require('../controllers/users/users.controller');
+// //const { addSymbolValidator } = require('../controllers/users/users.validators');
+// const { middleware: db } = require('../middlewares/db');
+// const enforeAuth = require('../middlewares/enforce-auth');
+// const enforceGuest = require('../middlewares/enforce-guests');
+// const mongo = require('../middlewares/mongo');
+
+// const joi = require('../middlewares/joi');
+
+// router.use(db);
+// router.use(mongo);
+// router.get('/welcome', enforceGuest, welcome);
+// router.get('/dashboard', enforeAuth, dashboard);
+// router.get('/logout', enforeAuth, logout);
+
+// //router.post('/symbol', joi(addSymbolValidator), addSymbol);
+// router.post('/symbol', addSymbol);
+
+// module.exports = router;
